d93407fbcdd0ead64d05930f1af00880b5185428
cristianetaniguti <chaytaniguti@gmail.com>
2021-03-28 00:07:40 -0300

    fix cred

--- R/app_server.R
+++ R/app_server.R
@@ -25,9 +25,13 @@ app_server <- function( input, output, session ) {
     stringsAsFactors = FALSE
   )
   
-  res_auth <- secure_server(
-    check_credentials = check_credentials(credentials)
+  res_auth <- shinymanager::secure_server(
+    check_credentials = shinymanager::check_credentials(credentials)
   )
+  
+  output$auth_output <- renderPrint({
+    reactiveValuesToList(res_auth)
+  })
 
   callModule(mod_met_server, "met_ui_1")
  
